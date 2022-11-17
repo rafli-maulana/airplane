3f6dd1b5b4025d9e01855233bb6eee00d9c48432
TTI-DCS <rafli.maulana@toeitechno.com>
2022-11-17 18:16:27 +0700

    add some file

--- lib/ui/pages/detail_page.dart
+++ lib/ui/pages/detail_page.dart
@@ -1,4 +1,5 @@
 import 'package:airplane/shared/theme.dart';
+import 'package:airplane/widgets/photo_item.dart';
 import 'package:flutter/material.dart';
 
 class DetailPage extends StatelessWidget {
@@ -38,12 +39,162 @@ class DetailPage extends StatelessWidget {
       );
     }
 
+    Widget content() {
+      return Container(
+        width: double.infinity,
+        margin: EdgeInsets.symmetric(
+          horizontal: defaultMargin,
+          vertical: defaultMargin,
+        ),
+        child: Column(
+          children: [
+            //NOTE EMBLEM
+            Container(
+              margin: EdgeInsets.only(
+                top: 30,
+              ),
+              width: 108,
+              height: 24,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(
+                    'assets/icon_emblem.png',
+                  ),
+                ),
+              ),
+            ),
+            // NOTE: TITLE
+            Container(
+              margin: EdgeInsets.only(
+                top: 256,
+              ),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Lake Ciliwung',
+                          style: whiteStyleText.copyWith(
+                              fontSize: 24, fontWeight: semiBold),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        Text(
+                          'Tangerang',
+                          style: whiteStyleText.copyWith(
+                            fontSize: 16,
+                            fontWeight: light,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        width: 20,
+                        height: 20,
+                        margin: EdgeInsets.only(right: 2),
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage(
+                              'assets/icon_star.png',
+                            ),
+                          ),
+                        ),
+                      ),
+                      Text(
+                        '4.8',
+                        style: whiteStyleText.copyWith(
+                          fontWeight: medium,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            // NOTE: DESCRIPTION
+            Container(
+              width: double.infinity,
+              margin: EdgeInsets.only(
+                top: 30,
+              ),
+              padding: EdgeInsets.symmetric(
+                horizontal: 20,
+                vertical: 30,
+              ),
+              decoration: BoxDecoration(
+                color: kWhiteColor,
+                borderRadius: BorderRadius.circular(
+                  18,
+                ),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  //NOTE: ABOUT
+                  Text(
+                    'About',
+                    style: blackStyleText.copyWith(
+                      fontSize: 16,
+                      fontWeight: semiBold,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 6,
+                  ),
+                  Text(
+                    'Berada di jalur jalan provinsi yang menghubungkan Denpasar Singaraja serta letaknya yang dekat dengan Kebun Raya Eka Karya menjadikan tempat Bali.',
+                    style: blackStyleText.copyWith(
+                      fontWeight: regular,
+                      height: 2,
+                    ),
+                  ),
+                  //NOTE: PHOTOS
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    'Photos',
+                    style: blackStyleText.copyWith(
+                      fontSize: 16,
+                      fontWeight: semiBold,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 6,
+                  ),
+                  Row(
+                    children: [
+                      PhotoItem(
+                        imageUrl: 'assets/image_photo1.png',
+                      ),
+                      PhotoItem(
+                        imageUrl: 'assets/image_photo2.png',
+                      ),
+                      PhotoItem(
+                        imageUrl: 'assets/image_photo3.png',
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      );
+    }
+
     return Scaffold(
       backgroundColor: kBackgroundColor,
       body: Stack(
         children: [
           backgroundImage(),
           customShadow(),
+          content(),
         ],
       ),
     );
